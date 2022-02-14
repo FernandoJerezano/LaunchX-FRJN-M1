c26a8f49f6c1b7ce0f2341a9ce675cd876ead274
Fernando Jerezano <fernando._jerezano@hotmail.com>
2022-02-14 14:53:05 -0600

    Katas

---
+++ modulo1_7_1.py
@@ -0,0 +1,14 @@
+
+
+# Declaramos 2 variables
+
+new_planet = ''
+planets = []
+
+# Escribe el ciclo while solicitado
+
+while new_planet.lower() != 'done':
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input('Enter a new planet ')
+
